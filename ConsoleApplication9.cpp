940f7291387303daf60994541f375e2b55a44a80
chertousovapolina <115408269+chertousovapolina@users.noreply.github.com>
2022-11-10 00:39:09 +0300

    Add files via upload

---
+++ ConsoleApplication9.cpp
@@ -0,0 +1,19 @@
+﻿// ConsoleApplication9.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+// Чертоусова последовательность
+#include <iostream>
+using namespace std;
+int main()
+{
+    float x, s=0,n=0;
+    
+    while(true) {
+        cin >> x;
+        if (x == 0) break;
+        s += x;
+        n++;
+    }
+    cout << s/n<<endl;
+    
+}
+
